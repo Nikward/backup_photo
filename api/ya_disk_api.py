eadfba465a6e427c90af8fb74d6a159db8d1364c
Nikolay Zolotuhin <nik.ward@yandex.ru>
2023-02-05 23:19:02 +0300

    Changed the formatting

--- api/ya_disk_api.py
+++ api/ya_disk_api.py
@@ -1,12 +1,14 @@
 import requests
-from data import ya_token
+
 
 class YandexApi:
     url = 'https://cloud-api.yandex.net'
+
     def __init__(self, token, name_folder):
         self.name_folder = name_folder
         self.token = token
         self.create_new_folder()
+
     def create_new_folder(self):
         specific_url = '/v1/disk/resources'
         metod_url = self.url + specific_url
@@ -17,7 +19,7 @@ class YandexApi:
         create_folder = requests.put(metod_url, params=params, headers=headers)
         print(f'Папка "{self.name_folder}" успешно создана.')
 
-    def upload_photo(self,name_file, url):
+    def upload_photo(self, name_file, url):
         specific_url = '/v1/disk/resources/upload'
         metod_url = self.url + specific_url
         params = {'path': f'{self.name_folder}/{name_file}',
@@ -28,6 +30,4 @@ class YandexApi:
                    'Authorization': self.token
                    }
 
-        save_file = requests.post(metod_url, params=params,headers=headers)
-
-
+        save_file = requests.post(metod_url, params=params, headers=headers)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from api.vk_api import VkUser
-from data import vk_token
-from api.ya_disk_api import YandexApi
-from data import ya_token
 from time import sleep
 import json
+
 from tqdm import tqdm
+
+from api.vk_api import VkUser
+from api.ya_disk_api import YandexApi
+from data import vk_token, ya_token
+
 
 def count_photo(obj):
     count = 0
@@ -14,15 +16,20 @@ def count_photo(obj):
 
 
 if __name__ == '__main__':
-    user_id = int(input('Введите id пользователя Вконтакте: '))
-    ver_api = float(input('Введите версию api: '))
-    user = VkUser(vk_token, user_id, ver_api, count=5)
+    user_id = input('Введите id или screen_name пользователя Вконтакте: ')
+    amount_photo = int(input('Укажите количество загружаемых фото: '))
+
+    user = VkUser(vk_token, user_id=user_id, count=amount_photo)
+
     fin_list = []
+
     ya_disk = YandexApi(ya_token, f'id_{user_id}')
+
     for photo in tqdm(user.get_data(), total=count_photo(user)):
-        ya_disk.upload_photo(photo[0],photo[1])
+        ya_disk.upload_photo(photo[0], photo[1])
         sleep(0.1)
         photo_info = {'file_name': f'{photo[0]}.jpg', 'size': photo[-1]}
         fin_list.append(photo_info)
+
     with open('information.json', "a") as file:
         json.dump(fin_list, file, indent=2)
